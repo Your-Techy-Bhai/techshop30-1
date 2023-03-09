49f5af1b60a33d8f2696b074384d509dfc4d35e8
TechyBhai1 <techyid3@gmail.com>
2023-03-09 16:31:39 +0530

    modified

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ $password = md5($_POST["password"]);
   session_start();
   $_SESSION['loggedin'] = true;
   $_SESSION['username'] = $username;
-  header("Location: Spotify");
+  header("Location: Spotify/index.html");
     }
 else{
     $showError = "Invalid Credentials";}
